8c0ac66ab08edc9beaba5d6a7d2b36be2c9bbc25
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-09 09:40:14 +0200

    Added Navigation components

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPage';
-function App() {
-  return <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Router path="/places/new" exact>
-        <NewPlace />
-      </Router>
-    </Switch>
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
+const App = () => {
+  return <Router>
+    <MainNavigation />
+    <main>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Router path="/places/new" exact>
+          <NewPlace />
+        </Router>
+      </Switch>
+    </main>
     <Redirect to='/' />
   </Router>
 }
