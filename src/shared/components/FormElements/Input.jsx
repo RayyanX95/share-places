ddc2c460bda05081431df0fa2f3c5f919a375cd4
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-16 09:42:34 +0200

    ShareInput Values & Adding Multiple Inputs & add useReducer

--- src/shared/components/FormElements/Input.jsx
+++ src/shared/components/FormElements/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useEffect } from 'react';
 
 import { validate } from '../../util/validators';
 import './Input.css';
@@ -28,6 +28,13 @@ const Input = (props) => {
       isValid: false,
       isTouched: false
     });
+
+    const {  id, onInput } = props;
+    const { value, isValid } = inputState;
+
+    useEffect(() => {
+      onInput(id, value, isValid);
+    }, [id, value, isValid, onInput]);
 
   const onChangeHandler = e => {
     dispatch({ type: 'CHANGE', val: e.target.value, validators: props.validators });
