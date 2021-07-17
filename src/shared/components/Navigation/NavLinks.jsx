242be693c637ffed1d193d8e4a5870579f2d5702
Ibarhim <ibrahim.alrayany@gmail.com>
2021-07-17 12:48:29 +0200

    Add LOGOUT button

--- src/shared/components/Navigation/NavLinks.jsx
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -21,6 +21,9 @@ const NanLinks = () => {
       {!auth.isLoggedIn && <li>
         <NavLink to="/auth" >Authenticate</NavLink>
       </li>}
+      {auth.isLoggedIn && <li>
+        <button onClick={auth.logout} >LOGOUT</button>
+      </li>}
     </ul >
   )
 }
