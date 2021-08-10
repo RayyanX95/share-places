711f219251c6def2a92ce2f20c76c0189230484c
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2021-08-10 07:55:58 +0200

    fix validation issue of password length

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -125,8 +125,8 @@ const Auth = () => {
             element="input"
             type="password"
             label="Password"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid password at least five character"
+            validators={[VALIDATOR_MINLENGTH(6)]}
+            errorText="Please enter a valid password at least 6 character"
             onInput={inputHandler}
           />
 
