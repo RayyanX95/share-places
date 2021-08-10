74f97fe740330657ac2f3d23808ee9a70793654c
Ibrahim AlRayyan <ibrahim.alrayany@gmail.com>
2021-08-10 07:29:14 +0200

    update place using real BE Api

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,56 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
+import './PlaceForm.css';
 import Input from '../../shared/components/FormElements/Input';
 import { VALIDATOR_REQUIRE } from './../../shared/util/validators';
 import Button from './../../shared/components/FormElements/Button';
-
-
-import './PlaceForm.css';
 import { useForm } from './../../shared/hooks/form-hook';
 import Card from '../../shared/components/UIElements/Card';
-
-export const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'First Place',
-    description: 'First place of Ibrahim AlRayyan house',
-    imageUrl: 'https://bloximages.newyork1.vip.townnews.com/thedesertreview.com/content/tncms/assets/v3/editorial/f/fb/ffb5de7a-eff4-11e7-8c16-23a73bf85f92/5a4be0b0b2cae.image.jpg',
-    address: 'New York, NY 10036, USA!',
-    creatorId: 'u1',
-    location: {
-      lat: 40.7580013,
-      lng: -73.9877574
-    },
-  },
-  {
-    id: 'p2',
-    title: 'First Place',
-    description: 'Second place of Ibrahim AlRayyan house',
-    imageUrl: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-witness/d/d6/River_Obelisk.jpg',
-    address: 'New York, NY 10036, USA!',
-    creatorId: 'u1',
-    location: {
-      lat: 40.7580013,
-      lng: -73.9877574
-    }
-  },
-  {
-    id: 'p2',
-    title: 'First Place',
-    description: 'Second place of Ibrahim AlRayyan house',
-    imageUrl: 'https://oyster.ignimgs.com/mediawiki/apis.ign.com/the-witness/d/d6/River_Obelisk.jpg',
-    address: 'New York, NY 10036, USA!',
-    creatorId: 'u3',
-    location: {
-      lat: 40.7580013,
-      lng: -73.9877574
-    }
-  }
-]
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from './../../shared/context/auth-context';
 
 const UpdatePlace = props => {
-  const [loading, setLoading] = useState(true);
+  const { isLoading, sendRequest, error, clearError } = useHttpClient();
+  const [loadedPlace, setLoadedPlace] = useState();
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+
+
   const placeId = useParams().placeId;
 
   const [formState, inputHandler, setFormData] = useForm({
@@ -64,36 +30,64 @@ const UpdatePlace = props => {
     }
   });
 
-  const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
-
   useEffect(() => {
-    if (identifiedPlace) {
-      setFormData(
-        {
-          title: {
-            value: identifiedPlace.title,
-            isValid: true,
-          },
-          description: {
-            value: identifiedPlace.description,
-            isValid: true,
-          }
-        },
-        true
-      );
-    }
-    setLoading(false);
-  }, [setFormData, identifiedPlace]);
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`);
+        setLoadedPlace(responseData.place);
+
+        // TODO fix issue of setFormData, not working correctly
+        // if (responseData.place) {
+        //   setFormData(
+        //     {
+        //       title: {
+        //         value: "responseData.place.title",
+        //         isValid: true,
+        //       },
+        //       description: {
+        //         value: "responseData.place.description",
+        //         isValid: true,
+        //       }
+        //     },
+        //     true
+        //   );
+        // }
+
+      } catch (error) {
+        // * Errors handled on useHttpClient
+      };
+    };
+    fetchPlace();
+
+  }, [setFormData, sendRequest, placeId]);
 
 
 
-  const placeUpdateSubmitHandler = e => {
+  const placeUpdateSubmitHandler = async e => {
     e.preventDefault();
 
-    console.log("UpdatePlace: ", formState.inputs);
+    try {
+      const responseData = await sendRequest(
+        `http://localhost:5000/api/places/${placeId}`,
+        'PATCH',
+        JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }),
+        {
+          'Content-Type': 'application/json',
+        });
+
+      history.push('/' + auth.userId + '/places');
+    } catch (error) {
+
+    };
   }
 
-  if (!identifiedPlace) {
+  if (isLoading) {
+    return <h1>loading...</h1>
+  }
+  if (!loadedPlace) {
     return (
       <Card>
         <div className="center" >
@@ -102,10 +96,6 @@ const UpdatePlace = props => {
       </Card>
     )
   };
-
-  if (loading) {
-    return <h1>loading...</h1>
-  }
 
   return (
     <form className="place-form" onSubmit={placeUpdateSubmitHandler} >
@@ -117,8 +107,8 @@ const UpdatePlace = props => {
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title"
         onInput={inputHandler}
-        initialValue={formState.inputs.title.value}
-        initValid={formState.inputs.title.isValid}
+        initialValue={loadedPlace.title}
+        initValid={true}
       />
       <Input
         id="description"
@@ -128,8 +118,8 @@ const UpdatePlace = props => {
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid description"
         onInput={inputHandler}
-        initialValue={formState.inputs.description.value}
-        initValid={formState.inputs.description.isValid}
+        initialValue={loadedPlace.description}
+        initValid={true}
       />
       <Button type="submit" disabled={!formState.isValid} >UPDATE PLACE</Button>
     </form>
